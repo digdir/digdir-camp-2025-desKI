e7491c2c0a2e56d67fefa05eb9516db90aa17690
johannegg <johanne.gronlien.gjedrem@digdir.no>
2025-06-18 10:13:07 +0200

    Format: Run ruff

--- app/services/llm_service.py
+++ app/services/llm_service.py
@@ -1,4 +1,5 @@
 # TODO: Bytt ut med OpenAI, Ollama eller lignende
 
+
 def generate_answer(prompt: str) -> str:
-    return "Dette er et generert svar fra LLM (dummy)."
+    return 'Dette er et generert svar fra LLM (dummy).'
