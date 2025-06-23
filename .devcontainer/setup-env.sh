c6b74c6bb6e2a308b2129b954b8816dfc26bbeee
Brinje Marie Haugli <brinje.marie.haugli@digdir.no>
2025-06-23 11:56:45 +0200

    Fix: move setup to env.setup

--- .devcontainer/setup-env.sh
+++ .devcontainer/setup-env.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo " Sjekker om .env eksisterer..."
 
 if [ ! -f .env ] && [ -f .env.template ]; then
     cp .env.template .env
-    echo ".env opprettet fra .env.template"
+    echo " .env created from .env.template"
+elif [ -f .env ]; then
+    echo " .env already exists, skipping creation."
 else
-    echo ".env finnes allerede, eller .env.template mangler"
+    echo ".env.template not found, please create a .env file manually."
 fi
-
 
