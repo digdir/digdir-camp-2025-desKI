9c38fa61be5fd7566858806dd20133d0592d0387
Brinje Marie Haugli <brinje.marie.haugli@digdir.no>
2025-06-23 14:12:00 +0200

    fixed

--- .devcontainer/setup-env.sh
+++ .devcontainer/setup-env.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
-
-
-if [ ! -f .env ] && [ -f .env.template ]; then
+#!/bin/sh
+if [ ! -f .env ] && [ -f .env.template ];then
     cp .env.template .env
-    echo " .env created from .env.template"
-elif [ -f .env ]; then
-    echo " .env already exists, skipping creation."
+    echo ".env created from .env.template"
 else
-    echo ".env.template not found, please create a .env file manually."
+    if [ -f .env ];then
+        echo ".env already exists, skipping creation."
+    else
+        echo ".env.template not found, please create a .env file manually."
+    fi
 fi
-
