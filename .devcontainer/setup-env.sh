ec7626a84efce29a0f063ee5e7a01969d880b0e4
Brinje Marie Haugli <159816205+brinjemarie@users.noreply.github.com>
2025-06-24 10:50:34 +0200

    18 bug git error (#19)

--- .devcontainer/setup-env.sh
+++ .devcontainer/setup-env.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
-
-echo " Sjekker om .env eksisterer..."
-
-if [ ! -f .env ] && [ -f .env.template ]; then
+#!/bin/sh
+if [ ! -f .env ] && [ -f .env.template ];then
     cp .env.template .env
-    echo ".env opprettet fra .env.template"
+    echo ".env created from .env.template"
 else
-    echo ".env finnes allerede, eller .env.template mangler"
+    if [ -f .env ];then
+        echo ".env already exists, skipping creation."
+    else
+        echo ".env.template not found, please create a .env file manually."
+    fi
 fi
-
-
