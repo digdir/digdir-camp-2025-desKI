5a4b11553d961f76d599956da9aff0b74b1ba371
magnus-lower <magnus.lower@gmail.com>
2025-06-30 16:00:50 +0200

    Included comments in the api_help_util.py file

--- app/utils/api_help_util.py
+++ app/utils/api_help_util.py
@@ -2,22 +2,16 @@ from urllib.parse import urlparse
 
 import requests
 
-
+# Ping the specified host to check if it is reachable
 def ping(url: str) -> bool:
-    """
-    Private method to ping the specified host to check if it is reachable.
-
-    Args:
-        url (str): The hostname or IP address to ping.
-
-    Returns:
-        bool: True if the host is reachable, False otherwise.
-    """
     parsed = urlparse(url)
     host = parsed.hostname
     port = parsed.port
     try:
+        # Send a GET request to the /ready endpoint
         response = requests.get(f'{host}:{port}/ready', timeout=3)
+        # Return True if the status code indicates success
         return response.status_code < 400
     except requests.RequestException:
+        # Return False if the request failed
         return False
