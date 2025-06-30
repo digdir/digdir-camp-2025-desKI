7e5739e44664a1098b53b7822022204df4c13013
magnus-lower <magnus.lower@gmail.com>
2025-06-30 15:46:59 +0200

    Included comments in the request_models.py file

--- app/models/request_models.py
+++ app/models/request_models.py
@@ -3,11 +3,11 @@ from pydantic import Field, BaseModel, field_validator
 
 from app.utils.desensitize import detect_sensitive_data
 
-
+# Base model for chat requests
 class BaseChatRequest(BaseModel):
     question: str = Field(..., min_length=2, max_length=500)
 
-
+# Extended model with input validation for sensitive data
 class StrictChatRequest(BaseChatRequest):
     @field_validator('question')
     def check_for_sensitive_input(cls, v):
