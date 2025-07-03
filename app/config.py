477fe6760edcf28e073cac3a8e1145a0743cc6a5
Vegard Aa Albretsen <599002@stud.hvl.no>
2025-07-03 14:18:13 +0200

    moved config stuff from env to config

---
+++ app/config.py
@@ -0,0 +1,6 @@
+CHROMA_PATH='app/db/chroma_db'
+COLLECTION_NAME='dig_docs'
+
+AZURE_ENDPOINT='https://digdircamp-resource.services.ai.azure.com/models'
+# Example model, change to the one you want to use
+AZURE_MODEL='Llama-4-Maverick-17B-128E-Instruct-FP8'
