f29e3bc0b80d4da1517876ca72c11090c67f9742
Magnus Løwer <148643539+magnus-lower@users.noreply.github.com>
2025-07-03 14:36:02 +0200

    Pass client and scope to the backend (#41)

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import ValidationError
 from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.exceptions import (
     validation_exception_handler,
@@ -13,6 +14,15 @@ from app.api.customersupport import router as customersupport
 
 # Create the FastAPI application
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],  # React app URL
+    allow_credentials=True,
+    allow_methods=['GET', 'POST'],
+    allow_headers=['*'],
+)
 
 # Include routers with their prefixes
 app.include_router(customersupport, prefix='/customersupport')
