e539e851dbfc5f3d7b8395bae9127b930923437d
Vegard Aa Albretsen <599002@stud.hvl.no>
2025-07-16 09:46:43 +0200

    small fix in services. Removed unused api endpoint

--- app/services/llm_service.py
+++ app/services/llm_service.py
@@ -7,7 +7,7 @@ import requests
 from dotenv import load_dotenv
 from azure.ai.inference import ChatCompletionsClient
 from azure.core.credentials import AzureKeyCredential
-from azure.ai.inference.models import UserMessage
+from azure.ai.inference.models import UserMessage, SystemMessage
 
 from app.config import (
     TOP_P,
--- app/services/query_service.py
+++ app/services/query_service.py
@@ -150,14 +150,17 @@ class QueryService:
             logger.info(f'retrieved {len(retrieved_context)} context chunks')
 
         except Exception as e:
-            logger.error(f'Error retrieving the context from ChromaDB: {e}')
-            return 'An error occured while retrieving documents'
+            # Handle the exception, e.g., log it or return an error message
+            logger.error(f'Error retrieving context from ChromaDB: {e}')
 
         try:
-            context_str = ''.join(retrieved_context)
-            return self.llm_service.generate_response(
-                user_query, context_str, named_endpoint, external_context
+            response = self.llm_service.generate_response_azure(
+                user_query=user_query,
+                retrieved_context=retrieved_context,
+                named_endpoint=named_endpoint,
+                external_context=external_context,
             )
+            return response
 
         except Exception as e:
             logger.error(f' Error generating response from LLM {e}')
