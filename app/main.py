97f8c5fe8160dabc31709ee11bb5f326826e3c65
Brinje Marie Haugli <159816205+brinjemarie@users.noreply.github.com>
2025-07-23 13:09:29 +0200

    feat: add a vector db for brukerstotte documentation (#85)

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ def startup_event():
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000'],
+    allow_origins=['http://localhost:3000', 'http://localhost:5173'],
     allow_credentials=True,
     allow_methods=['GET', 'POST'],
     allow_headers=['*'],
