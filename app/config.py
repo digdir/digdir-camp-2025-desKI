07997c262a5839c75d59506708310ec8886244a4
Brinje Marie Haugli <brinje.marie.haugli@digdir.no>
2025-07-23 12:30:28 +0200

    Fix: ruff format

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 # === Vector db ====
 
-#CHROMA_PATH = 'app/db/chroma_db'
-#COLLECTION_NAME = 'brukerstotte_docs'
-#DATA_PATH = '_docs/brukerstotteDocs'
+# CHROMA_PATH = 'app/db/chroma_db'
+# COLLECTION_NAME = 'brukerstotte_docs'
+# DATA_PATH = '_docs/brukerstotteDocs'
 
 CHROMA_PATH = 'app/db/chroma_db'
 COLLECTION_NAME = 'dig_docs'
