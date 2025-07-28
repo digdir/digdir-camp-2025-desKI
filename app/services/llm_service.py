c8d7e9be9fece07f1756d4b1f1b423ab6d4c390a
Kaja Hovinbøle <127496504+kajahovinbole@users.noreply.github.com>
2025-07-28 14:40:43 +0200

    Testing small variations finetuned model (#102)

--- app/services/llm_service.py
+++ app/services/llm_service.py
@@ -230,6 +230,9 @@ class LLMService:
 
         logger.info(f'User info: {external_context}')
         logger.info(f'User Endpoint: {named_endpoint}')
+        prompt = PromptFactory.get_prompt(
+            user_query, retrieved_context, named_endpoint, external_context
+        )
         logger.info(f'Generated prompt (first 500 chars): {prompt}')
 
         session = requests.Session()
@@ -249,6 +252,7 @@ class LLMService:
             )
             response.raise_for_status()
             data = response.json()
+
             return data.get('response', '[No response]')
         except Exception as e:
             logger.error(f'Error generating response (finetuned): {e}')
